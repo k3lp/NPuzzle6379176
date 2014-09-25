bd4f88e81294d744621937ba6c0000746b30d126
hiddehensel <hiddehensel@gmail.com>
2014-09-26 00:09:44 +0200

    imageviews voor normal/hard
    wel heel lelijk

--- nPuzzle6379176/src/nl/mprog/projects/nPuzzle6379176/GameActivity.java
+++ nPuzzle6379176/src/nl/mprog/projects/nPuzzle6379176/GameActivity.java
@@ -46,8 +46,8 @@ import android.view.MenuItem;
     	moeilijkheid = getIntent().getExtras().getInt("moeilijkheid");
     	imgnr = getIntent().getExtras().getInt("imagebm");
     	
-        reqWidth = (int) (getResources().getDisplayMetrics().widthPixels * 0.9);
-        reqHeight= (int) (getResources().getDisplayMetrics().widthPixels * 0.9);
+        reqWidth = (int) (getResources().getDisplayMetrics().widthPixels * 0.8);
+        reqHeight= (int) (getResources().getDisplayMetrics().widthPixels * 0.8);
         
     	//vraag img op met imgnr
     	LinearLayout gallery1 = (LinearLayout)findViewById(R.id.gallery1);
@@ -222,17 +222,25 @@ import android.view.MenuItem;
     	Bitmap stuk1 = Bitmap.createBitmap(scaledimgbm, 1,1,pt1,pt2);
     	Bitmap stuk2 = Bitmap.createBitmap(scaledimgbm, pt1,1,pt1,pt2);
     	Bitmap stuk3 = Bitmap.createBitmap(scaledimgbm, pt1*2,1,pt1,pt2);
+    	Bitmap stuk4 = Bitmap.createBitmap(scaledimgbm, pt1*3,1,pt1,pt2);
     	
     	//bitmaps stukken midden
     	Bitmap stuk122 = Bitmap.createBitmap(scaledimgbm, 1,pt2,pt1,pt2);
     	Bitmap stuk222 = Bitmap.createBitmap(scaledimgbm, pt1,pt2,pt1,pt2);
     	Bitmap stuk322 = Bitmap.createBitmap(scaledimgbm, pt1*2,pt2,pt1,pt2);
-
+    	Bitmap stuk422 = Bitmap.createBitmap(scaledimgbm, pt1*3,pt2,pt1,pt2);
+    	
+    	//bitmaps stukken midden2
+    	Bitmap stuk133 = Bitmap.createBitmap(scaledimgbm, 1,pt2*2,pt1,pt2);
+    	Bitmap stuk233 = Bitmap.createBitmap(scaledimgbm, pt1,pt2*2,pt1,pt2);
+    	Bitmap stuk333 = Bitmap.createBitmap(scaledimgbm, pt1*2,pt2*2,pt1,pt2);
+    	Bitmap stuk433 = Bitmap.createBitmap(scaledimgbm, pt1*3,pt2*2,pt1,pt2);
     	
     	//bitmaps stukken onder
-    	Bitmap stuk133 = Bitmap.createBitmap(scaledimgbm, 1,pt2+pt2,pt1,pt2);
-    	Bitmap stuk233 = Bitmap.createBitmap(scaledimgbm, pt1,pt2+pt2,pt1,pt2);
-    	Bitmap stuk333 = Bitmap.createBitmap(scaledimgbm, pt1*2,pt2+pt2,pt1,pt2);
+    	Bitmap stuk144 = Bitmap.createBitmap(scaledimgbm, 1,pt2*3,pt1,pt2);
+    	Bitmap stuk244 = Bitmap.createBitmap(scaledimgbm, pt1,pt2*3,pt1,pt2);
+    	Bitmap stuk344 = Bitmap.createBitmap(scaledimgbm, pt1*2,pt2*3,pt1,pt2);
+    	Bitmap stuk444 = Bitmap.createBitmap(scaledimgbm, pt1*3,pt2*3,pt1,pt2);
     	
     	//klaar met volledige bm, recycle
     	//scaledimgbm.recycle();
@@ -246,6 +254,9 @@ import android.view.MenuItem;
     	ImageView vImg13;
     	vImg13 = (ImageView) findViewById(R.id.h13);
     	vImg13.setImageBitmap(stuk3);
+    	ImageView vImg14;
+    	vImg14 = (ImageView) findViewById(R.id.h14);
+    	vImg14.setImageBitmap(stuk4);
     	
     	ImageView vImg21;
     	vImg21 = (ImageView) findViewById(R.id.h21);
@@ -256,6 +267,9 @@ import android.view.MenuItem;
     	ImageView vImg23;
     	vImg23 = (ImageView) findViewById(R.id.h23);
     	vImg23.setImageBitmap(stuk322);
+    	ImageView vImg24;
+    	vImg24 = (ImageView) findViewById(R.id.h24);
+    	vImg24.setImageBitmap(stuk422);
     	
     	ImageView vImg31;
     	vImg31 = (ImageView) findViewById(R.id.h31);
@@ -266,12 +280,27 @@ import android.view.MenuItem;
     	ImageView vImg33;
     	vImg33 = (ImageView) findViewById(R.id.h33);
     	vImg33.setImageBitmap(stuk333);
+    	ImageView vImg34;
+    	vImg34 = (ImageView) findViewById(R.id.h34);
+    	vImg34.setImageBitmap(stuk433);
+    	
+    	ImageView vImg41;
+    	vImg41 = (ImageView) findViewById(R.id.h41);
+    	vImg41.setImageBitmap(stuk144);
+    	ImageView vImg42;
+    	vImg42 = (ImageView) findViewById(R.id.h42);
+    	vImg42.setImageBitmap(stuk244);
+    	ImageView vImg43;
+    	vImg43 = (ImageView) findViewById(R.id.h43);
+    	vImg43.setImageBitmap(stuk344);
+    	ImageView vImg44;
+    	vImg44 = (ImageView) findViewById(R.id.h44);
+    	vImg44.setImageBitmap(stuk444);
 	}
 	
 	
 	void maakStuks5(Bitmap scaledimgbm)
-	{
-		//aantal tiles
+	{		//aantal tiles
     	int dimensieTiles = moeilijkheid + 3;
     	int aantalTiles = dimensieTiles * dimensieTiles;
         
@@ -282,17 +311,36 @@ import android.view.MenuItem;
     	Bitmap stuk1 = Bitmap.createBitmap(scaledimgbm, 1,1,pt1,pt2);
     	Bitmap stuk2 = Bitmap.createBitmap(scaledimgbm, pt1,1,pt1,pt2);
     	Bitmap stuk3 = Bitmap.createBitmap(scaledimgbm, pt1*2,1,pt1,pt2);
+    	Bitmap stuk4 = Bitmap.createBitmap(scaledimgbm, pt1*3,1,pt1,pt2);
+    	Bitmap stuk5 = Bitmap.createBitmap(scaledimgbm, pt1*4,1,pt1,pt2);
     	
     	//bitmaps stukken midden
     	Bitmap stuk122 = Bitmap.createBitmap(scaledimgbm, 1,pt2,pt1,pt2);
     	Bitmap stuk222 = Bitmap.createBitmap(scaledimgbm, pt1,pt2,pt1,pt2);
     	Bitmap stuk322 = Bitmap.createBitmap(scaledimgbm, pt1*2,pt2,pt1,pt2);
-
+    	Bitmap stuk422 = Bitmap.createBitmap(scaledimgbm, pt1*3,pt2,pt1,pt2);
+    	Bitmap stuk522 = Bitmap.createBitmap(scaledimgbm, pt1*4,pt2,pt1,pt2);
     	
-    	//bitmaps stukken onder
-    	Bitmap stuk133 = Bitmap.createBitmap(scaledimgbm, 1,pt2+pt2,pt1,pt2);
-    	Bitmap stuk233 = Bitmap.createBitmap(scaledimgbm, pt1,pt2+pt2,pt1,pt2);
-    	Bitmap stuk333 = Bitmap.createBitmap(scaledimgbm, pt1*2,pt2+pt2,pt1,pt2);
+    	//bitmaps stukken midden2
+    	Bitmap stuk133 = Bitmap.createBitmap(scaledimgbm, 1,pt2*2,pt1,pt2);
+    	Bitmap stuk233 = Bitmap.createBitmap(scaledimgbm, pt1,pt2*2,pt1,pt2);
+    	Bitmap stuk333 = Bitmap.createBitmap(scaledimgbm, pt1*2,pt2*2,pt1,pt2);
+    	Bitmap stuk433 = Bitmap.createBitmap(scaledimgbm, pt1*3,pt2*2,pt1,pt2);
+    	Bitmap stuk533 = Bitmap.createBitmap(scaledimgbm, pt1*4,pt2*2,pt1,pt2);
+    	
+    	//bitmaps stukken midden3
+    	Bitmap stuk144 = Bitmap.createBitmap(scaledimgbm, 1,pt2*3,pt1,pt2);
+    	Bitmap stuk244 = Bitmap.createBitmap(scaledimgbm, pt1,pt2*3,pt1,pt2);
+    	Bitmap stuk344 = Bitmap.createBitmap(scaledimgbm, pt1*2,pt2*3,pt1,pt2);
+    	Bitmap stuk444 = Bitmap.createBitmap(scaledimgbm, pt1*3,pt2*3,pt1,pt2);
+    	Bitmap stuk544 = Bitmap.createBitmap(scaledimgbm, pt1*4,pt2*3,pt1,pt2);
+    	
+    	//bitmaps stukken beneden
+    	Bitmap stuk155 = Bitmap.createBitmap(scaledimgbm, 1,pt2*4,pt1,pt2);
+    	Bitmap stuk255 = Bitmap.createBitmap(scaledimgbm, pt1,pt2*4,pt1,pt2);
+    	Bitmap stuk355 = Bitmap.createBitmap(scaledimgbm, pt1*2,pt2*4,pt1,pt2);
+    	Bitmap stuk455 = Bitmap.createBitmap(scaledimgbm, pt1*3,pt2*4,pt1,pt2);
+    	Bitmap stuk555 = Bitmap.createBitmap(scaledimgbm, pt1*4,pt2*4,pt1,pt2);
     	
     	//klaar met volledige bm, recycle
     	//scaledimgbm.recycle();
@@ -306,6 +354,12 @@ import android.view.MenuItem;
     	ImageView vImg13;
     	vImg13 = (ImageView) findViewById(R.id.h13);
     	vImg13.setImageBitmap(stuk3);
+    	ImageView vImg14;
+    	vImg14 = (ImageView) findViewById(R.id.h14);
+    	vImg14.setImageBitmap(stuk4);
+    	ImageView vImg15;
+    	vImg15 = (ImageView) findViewById(R.id.h15);
+    	vImg15.setImageBitmap(stuk5);
     	
     	ImageView vImg21;
     	vImg21 = (ImageView) findViewById(R.id.h21);
@@ -316,6 +370,12 @@ import android.view.MenuItem;
     	ImageView vImg23;
     	vImg23 = (ImageView) findViewById(R.id.h23);
     	vImg23.setImageBitmap(stuk322);
+    	ImageView vImg24;
+    	vImg24 = (ImageView) findViewById(R.id.h24);
+    	vImg24.setImageBitmap(stuk422);
+    	ImageView vImg25;
+    	vImg25 = (ImageView) findViewById(R.id.h25);
+    	vImg25.setImageBitmap(stuk522);
     	
     	ImageView vImg31;
     	vImg31 = (ImageView) findViewById(R.id.h31);
@@ -326,6 +386,44 @@ import android.view.MenuItem;
     	ImageView vImg33;
     	vImg33 = (ImageView) findViewById(R.id.h33);
     	vImg33.setImageBitmap(stuk333);
+    	ImageView vImg34;
+    	vImg34 = (ImageView) findViewById(R.id.h34);
+    	vImg34.setImageBitmap(stuk433);
+    	ImageView vImg35;
+    	vImg35 = (ImageView) findViewById(R.id.h35);
+    	vImg35.setImageBitmap(stuk533);
+    	
+    	ImageView vImg41;
+    	vImg41 = (ImageView) findViewById(R.id.h41);
+    	vImg41.setImageBitmap(stuk144);
+    	ImageView vImg42;
+    	vImg42 = (ImageView) findViewById(R.id.h42);
+    	vImg42.setImageBitmap(stuk244);
+    	ImageView vImg43;
+    	vImg43 = (ImageView) findViewById(R.id.h43);
+    	vImg43.setImageBitmap(stuk344);
+    	ImageView vImg44;
+    	vImg44 = (ImageView) findViewById(R.id.h44);
+    	vImg44.setImageBitmap(stuk444);
+    	ImageView vImg45;
+    	vImg45 = (ImageView) findViewById(R.id.h45);
+    	vImg45.setImageBitmap(stuk544);
+    	
+    	ImageView vImg51;
+    	vImg51 = (ImageView) findViewById(R.id.h51);
+    	vImg51.setImageBitmap(stuk155);
+    	ImageView vImg52;
+    	vImg52 = (ImageView) findViewById(R.id.h52);
+    	vImg52.setImageBitmap(stuk255);
+    	ImageView vImg53;
+    	vImg53 = (ImageView) findViewById(R.id.h53);
+    	vImg53.setImageBitmap(stuk355);
+    	ImageView vImg54;
+    	vImg54 = (ImageView) findViewById(R.id.h54);
+    	vImg54.setImageBitmap(stuk455);
+    	ImageView vImg55;
+    	vImg55 = (ImageView) findViewById(R.id.h55);
+    	vImg55.setImageBitmap(stuk555);
 	}
 	
 	//maak een menu bij de menubutton
